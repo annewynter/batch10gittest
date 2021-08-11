727be1995c5c6ec33d33891468ed67162b2e84af
annewynter <kushnareva86@gmail.com>
2021-08-11 19:36:08 -0400

    first commit of project

---
+++ src/com/syntax/git/gitfinalclass.java
@@ -0,0 +1,12 @@
+package com.syntax.git;
+
+public class gitfinalclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this is out first commit file");
+
+	}
+
+}
