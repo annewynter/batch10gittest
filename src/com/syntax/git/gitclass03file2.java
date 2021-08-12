9009a5caa33bae341f445a9781bfe230ea8730ac
annewynter <kushnareva86@gmail.com>
2021-08-11 21:24:12 -0400

    third commit

--- src/com/syntax/git/gitclass03file2.java
+++ src/com/syntax/git/gitclass03file2.java
@@ -5,6 +5,8 @@ public class gitclass03file2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("this is the second commit file");
+
+System.out.println("making changes");
 	}
 
 }
